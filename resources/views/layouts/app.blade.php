1aed5980b3f47789f1d80898f0f9644b996d055f
Amable Sebastian Quishpe <105821062+SebasDh56@users.noreply.github.com>
2025-07-14 19:50:34 -0500

    Deploy 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -122,16 +122,23 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     @auth
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="{{ route('personas.index') }}"><i class="bi bi-people-fill"></i> Personas</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="{{ route('cooperativas.index') }}"><i class="bi bi-building-fill"></i> Cooperativas</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="{{ route('ventas.index') }}"><i class="bi bi-receipt-cutoff"></i> Ventas</a>
-                        </li>
-                        @if (Auth::user()->role === 'admin')
+                        @if (Auth::user()->role === 'cliente')
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="{{ route('personas.index') }}"><i class="bi bi-people-fill"></i> Personas</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="{{ route('ventas.index') }}"><i class="bi bi-receipt-cutoff"></i> Ventas</a>
+                            </li>
+                        @elseif (Auth::user()->role === 'admin')
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="{{ route('personas.index') }}"><i class="bi bi-people-fill"></i> Personas</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="{{ route('cooperativas.index') }}"><i class="bi bi-building-fill"></i> Cooperativas</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="{{ route('ventas.index') }}"><i class="bi bi-receipt-cutoff"></i> Ventas</a>
+                            </li>
                             <li class="nav-item">
                                 <a class="nav-link text-white" href="{{ route('users.index') }}"><i class="bi bi-gear"></i> Gestionar Usuarios</a>
                             </li>
@@ -160,29 +167,8 @@
     </nav>
 
     <div class="container">
-        @if (session('success'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                <i class="bi bi-check-circle-fill me-2"></i>{{ session('success') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        @endif
-        @if (session('error'))
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                <i class="bi bi-exclamation-triangle-fill me-2"></i>{{ session('error') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        @endif
-        @if ($errors->any())
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                <i class="bi bi-exclamation-triangle-fill me-2"></i>
-                <ul class="mb-0">
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        @endif
+        
+       
 
         @yield('content')
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PersonaController;
 use App\Http\Controllers\CooperativaController;
@@ -8,17 +7,13 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\AssignRoleMiddleware;
 
-
-Auth::routes();
 Route::get('/', [HomeController::class, 'index'])->name('home');
-
-Route::middleware([AssignRoleMiddleware::class . ':admin,cliente'])->group(function () {
-    Route::resource('personas', PersonaController::class);
-    Route::resource('cooperativas', CooperativaController::class);
-    Route::resource('ventas', VentaController::class);
-    Route::put('cooperativas/{cooperativa}', [CooperativaController::class, 'update']);
-    Route::get('ventas/resumen', [VentaController::class, 'resumen'])->name('ventas.resumen');
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::put('/users/{id}/role', [UserController::class, 'updateRole'])->name('users.updateRole');
-    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-});
+Route::resource('personas', PersonaController::class)->middleware([AssignRoleMiddleware::class . ':admin,cliente']);
+Route::resource('cooperativas', CooperativaController::class)->middleware([AssignRoleMiddleware::class . ':admin']);
+Route::resource('ventas', VentaController::class)->middleware([AssignRoleMiddleware::class . ':admin,cliente']);
+Route::put('cooperativas/{cooperativa}', [CooperativaController::class, 'update'])->middleware([AssignRoleMiddleware::class . ':admin']);
+Route::get('ventas/resumen', [VentaController::class, 'resumen'])->name('ventas.resumen')->middleware([AssignRoleMiddleware::class . ':admin,cliente']);
+Route::get('/users', [UserController::class, 'index'])->name('users.index')->middleware([AssignRoleMiddleware::class . ':admin']);
+Route::put('/users/{id}/role', [UserController::class, 'updateRole'])->name('users.updateRole')->middleware([AssignRoleMiddleware::class . ':admin']);
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy')->middleware([AssignRoleMiddleware::class . ':admin']);
+Auth::routes();
